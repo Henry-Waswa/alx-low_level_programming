343be8dbf665a14d41b0cbc2ea10a1eb37f6a7c0
Henry-Waswa <55430679+Henry-Waswa@users.noreply.github.com>
2023-02-17 23:04:59 +0300

    Task 12.a program that prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,42 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
- *Return: Always 0 (Success)
- */
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
 
 int main(void)
 {
-	int tens;
-	int ones;
-	int t;
-	int o;
+	int p, q;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
+	for (p = 0; p <= 98; p++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		for (q = p + 1; q <= 99; q++)
 		{
-		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
-			{
-				for (o = ones + 1; o <= '9'; o++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
 
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				o = '0';
-			}
+			if (p == 98 && q == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
