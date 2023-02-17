644880a65a7b92a1c6e6d151286a461bdd015589
Henry-Waswa <55430679+Henry-Waswa@users.noreply.github.com>
2023-02-17 22:59:26 +0300

    Task 10. a program that prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
 /**
- *main - Entry point, print 00 to 99 using putchar
+ *main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
+ *
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int tens;
 	int ones;
+	int tens;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print tens place*/
+	for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
 	{
-		for (ones = '0'; ones <= '9'; ones++) /*print ones place*/
+		for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
 		{
 			putchar(tens);
 			putchar(ones);
-			if (!(tens == '9' && ones == '9')) /*skip comma at end*/
+
+			if (tens != '8' || ones != '9') /*print commas*/
 			{
 				putchar(',');
 				putchar(' ');
