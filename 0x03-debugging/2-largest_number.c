860890ca738c91b728d8400555eeba5d31b4a257
Henry-Waswa <55430679+Henry-Waswa@users.noreply.github.com>
2023-02-22 19:51:53 +0300

    Task 2.Fix the code in 2-largest_number.c so that it correctly prints out the largest of three numbers, no matter the case.

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,37 +1,28 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
  */
-
-void print_remaining_days(int month, int day, int year)
+int largest_number(int a, int b, int c)
 {
-	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
-	{
-		if (month >= 2)
-		{
-			day++;
-		}
+	int largest;
 
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
-	else
+	if (a >= b && a >= c)
 	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
+		largest = a;
 	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else if (c >= a && c >= b)
+	{
+		largest = c;
+	}
+
+	return (largest);
 }
