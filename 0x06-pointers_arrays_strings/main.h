d8bf42db05a45c613e044012bf6a5e7ea432dccf
Henry-Waswa <55430679+Henry-Waswa@users.noreply.github.com>
2023-03-01 18:23:31 +0300

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,9 +7,11 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *n);
-char *cap_string(char *n);
-char *leet(char *n);
-char *rot13(char *n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /* MAIN_H */
