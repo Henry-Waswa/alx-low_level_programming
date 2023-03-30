5243bbc0fff8af5f83567a80726ec987ef2a8c18
Henry-Waswa <55430679+Henry-Waswa@users.noreply.github.com>
2023-03-31 00:39:27 +0300

    Task 2-Adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,40 +1,21 @@
 #include "lists.h"
-#include <string.h>
-
+#include <stdlib.h>
 /**
- * add_node - Adds a new node at the beginning
- *            of a list_t list.
- * @head: A pointer to the head of the list_t list.
- * @str: The string to be added to the list_t list.
- *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new element.
- */
-list_t *add_node(list_t **head, const char *str)
+  * add_nodeint - add new nodes to the list
+  * @head: current place in the list
+  * @n: int to add to the list
+  * Return: pointer to current position in list
+  */
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	char *dup;
-	int len;
-	list_t *new;
+	listint_t *new;
 
-	new = malloc(sizeof(list_t));
+	new = *head;
+	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-
-	dup = strdup(str);
-	if (dup == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-
-	for (len = 0; str[len];)
-		len++;
-
-	new->str = dup;
-	new->len = len;
+	new->n = n;
 	new->next = *head;
-
 	*head = new;
-
-	return (new);
+	return (*head);
 }
