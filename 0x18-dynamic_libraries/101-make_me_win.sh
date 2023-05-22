a579c33d52a4b79cc8c46311e83a3292f79a3903
Henry-Waswa <55430679+Henry-Waswa@users.noreply.github.com>
2023-05-22 12:40:34 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 i#!/bin/bash
-wget -P /tmp https://github.com/Henry-Waswa/alx-low_level_programming/main/0x18-dynamic_libraries/putshack.so
+wget -P /tmp https://github.com/Henry-Waswa/alx-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
 export LD_PRELOAD="$PWD/../putshack.so"
